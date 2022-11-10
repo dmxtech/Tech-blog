88e0d32b596bae8a72442592270d0bdaf2b4819f
cjclap <dmxtechinn@gmail.com>
2022-11-09 21:49:10 -0600

    login and sign up wirking

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const userRoutes = require('./api/userRoutes');
+const homeRoutes = require('./homeRoutes');
 
-router.use('/', userRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 
