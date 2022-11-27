05d9c208db36dd5c7de64fb2bebdfa0179af87b8
cjclap <dmxtechinn@gmail.com>
2022-11-27 03:07:15 -0600

    working on sign up and updated delete button but only deletign top item?

--- public/js/profile.js
+++ public/js/profile.js
@@ -22,26 +22,26 @@ const newFormHandler = async (event) => {
     }
 };
 
-// const delButtonHandler = async (event) => {
-//     if (event.target.hasAttribute('delete-btn')) {
-//         const id = event.target.getAttribute('delete-btn');
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('delete-btn')) {
+        const id = event.target.getAttribute('delete-btn');
 
-//         const response = await fetch(`/api/posts/${id}`, {
-//             method: 'DELETE',
-//         });
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/profile');
-//         } else {
-//             alert('Failed to delete post');
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete post');
+        }
+    }
+};
 
 document
     .querySelector('.new-post-form')
     .addEventListener('submit', newFormHandler);
 
-// document
-//     .querySelector('.delete-btn')
-//     .addEventListener('click', delButtonHandler);
+document
+    .querySelector("#delete-btn")
+    .addEventListener("click", delButtonHandler);
